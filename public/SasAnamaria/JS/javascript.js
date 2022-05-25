54ce2c1fde948b5ba68e10b2cea6006cc5e3a56b
Anamaria Sas <Anamaria Sas@DESKTOP-MESSROT>
2022-05-25 19:24:09 +0300

    personal page

---
+++ public/SasAnamaria/JS/javascript.js
@@ -0,0 +1,4 @@
+const Name = document.querySelector('aside span');
+Name.onclick = () => {
+    window.open('https://github.com/anamaria-sas', '_blank');
+}
--- src/Controller/AncaAdrianController.php
+++ src/Controller/AncaAdrianController.php
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 <?php
 
-namespace App\Controller;
 
+
+namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,26 +13,7 @@ class AncaAdrianController extends AbstractController
     public function index(): Response
     {
         return $this->render('AncaAdrian/index.html.twig', [
-            'controller_name' => 'AncaAdrianController',
+                'controller_name' => 'AncaAdrianController',
         ]);
     }
-=======
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class AncaAdrianController extends AbstractController
-{
-    #[Route('/anca-adrian')]
-    public function index(): Response
-    {
-        return $this->render('AncaAdrian/index.html.twig', [
-            'controller_name' => 'AncaAdrianController',
-        ]);
-    }
->>>>>>> 097b9b46d4b34eb14d0f2e86c788c8211bf18647
 }
